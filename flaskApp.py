8520a29c66a5042aae9cf119050db6130ab49bc8
Sebastian Echeverry <uno.sebastian.echo@gmail.com>
2021-04-22 18:25:25 -0400

    initial bootstrap stub

---
+++ flaskApp.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect
+
+# Set up flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+	
+@app.route("/perState")
+def per_state():
+    return render_template("per_state.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/scrape")
+def scrape():
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
